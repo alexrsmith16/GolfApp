966ac8de772d2ffb3fd55ec10965e4ea280d1827
Alexander Smith <alexandersmith@Alexanders-MBP.veracitynetworks.net>
2019-11-23 22:52:48 -0700

    landing page finished

--- main.js
+++ main.js
@@ -1,15 +1,63 @@
-let numplayers = 5;
-let numholes = 18;
-function table() {
-    let toappend = "";
-    for (let i = 0; i < numholes; i++) {
-        toappend += `<div class="table-column">`;
-        for (let j = 0; j < numplayers; j++) {
-            toappend += `<div>${i},${j}</div>`;
-            console.log(j);
-        }
-        toappend += `</div>`;
-    }
-    $(".table").html(toappend);
+let COURSES = document.getElementById("courses");
+let cardHeaders = document.getElementsByClassName("card-header");
+let LANDING_HEADER = document.getElementById("landing_header");
+for (let i of cardHeaders) {
+    i.addEventListener('mouseenter', e => {
+        i.previousElementSibling.style.filter = "blur(2px)";
+    });
+
+    i.addEventListener('mouseleave', e => {
+        i.previousElementSibling.style.filter = "";
+    });
 }
-table();
+
+function get(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = () => resolve(JSON.parse(xhr.responseText));
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send();
+    });
+}
+
+function clickCard(element) {
+    LANDING_HEADER.classList.add("disappear");
+    COURSES.classList.add("playing-game");
+    element.classList.add("active");
+    switch (element.id) {
+        case 0:
+
+
+    }
+    startGame()
+}
+
+function startGame(test) {
+
+}
+
+
+
+
+
+
+
+
+
+
+// let numplayers = 5;
+// let numholes = 18;
+// function table() {
+//     let toappend = "";
+//     for (let i = 0; i < numholes; i++) {
+//         toappend += `<div class="table-column">`;
+//         for (let j = 0; j < numplayers; j++) {
+//             toappend += `<div>${i},${j}</div>`;
+//             console.log(j);
+//         }
+//         toappend += `</div>`;
+//     }
+//     $(".table").html(toappend);
+// }
+// table();
